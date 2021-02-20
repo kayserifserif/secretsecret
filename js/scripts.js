8311bff058afe1fe860aa336e8cfe5e7728c09c9
whykatherine <whykatherine@gmail.com>
2021-02-20 21:40:56 +0800

    feat: use localstorage

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,141 @@
 const write__btn = document.getElementById('write__btn');
-
 const write = document.getElementById('write');
 let textarea;
-let secret__btn;
 
+let secret__btn;
 const secret = document.getElementById('secret');
-let canvas;
 let submit_btn;
+let canvas;
+let text;
+let secretWordIndex;
+let dataURL;
 
 const secrets = document.getElementById('secrets');
+let secret_imgs = localStorage.getItem('secret_imgs');
+if (secret_imgs) {
+  try {
+    secret_imgs = JSON.parse(secret_imgs);
+    for (let secret_img of secret_imgs) {
+      let image = new Image();
+      image.src = secret_img.src;
+      image.style = secret_img.style;
+      image.classList.add('secret_img');
+      secrets.appendChild(image);
+    }
+  } catch {
+    secret_imgs = [];
+  }
+} else {
+  secret_imgs = [];
+}
 
 const f = new FontFace('Flow Circular', 'url(/fonts/flow-circular.woff)');
 f.load().then(() => write__btn.addEventListener('click', startWriting));
 
 function startWriting() {
-  textarea = document.createElement('textarea');
-  textarea.cols = '50';
-  textarea.rows = '20';
+  if (write.children.length === 0) {
+    textarea = document.createElement('textarea');
+    textarea.cols = '50';
+    textarea.rows = '20';
 
-  secret__btn = document.createElement('button');
-  secret__btn.type = 'button';
-  secret__btn.classList.add('button');
-  secret__btn.appendChild(document.createTextNode('secretttt'));
-  secret__btn.addEventListener('click', makeSecret);
+    secret__btn = document.createElement('button');
+    secret__btn.type = 'button';
+    secret__btn.classList.add('button');
+    secret__btn.appendChild(document.createTextNode('secretttt'));
+    secret__btn.addEventListener('click', makeSecret);
 
-  write.appendChild(textarea);
-  write.appendChild(secret__btn);
-  
-  $(textarea).hide();
-  $(secret__btn).hide();
-  $(textarea).fadeIn(500, () => textarea.focus());
-  $(secret__btn).fadeIn(500);
+    write.appendChild(textarea);
+    write.appendChild(secret__btn);
+    
+    $(textarea).hide();
+    $(secret__btn).hide();
+    $(textarea).fadeIn(500, () => textarea.focus());
+    $(secret__btn).fadeIn(500);
+  }
 }
 
 function makeSecret() {
-  let text = textarea.value;
+  if (secret.children.length === 0) {
+    text = textarea.value;
 
-  $(textarea).fadeOut(500, () => textarea.remove());
-  $(secret__btn).fadeOut(500, () => secret__btn.remove());
+    $(textarea).fadeOut(500, () => textarea.remove());
+    $(secret__btn).fadeOut(500, () => secret__btn.remove());
 
-  canvas = document.createElement('canvas');
-  canvas.classList.add('canvas');
-  canvas.width = 350;
-  canvas.height = 450;
+    canvas = document.createElement('canvas');
+    canvas.classList.add('canvas');
+    canvas.width = 350;
+    canvas.height = 500;
+    secret.append(canvas);
 
+    submit_btn = document.createElement('button');
+    submit_btn.type = 'button';
+    submit_btn.classList.add('button');
+    submit_btn.appendChild(document.createTextNode('submit'));
+    submit_btn.addEventListener('click', submitSecret);
+    secret.appendChild(submit_btn);
+
+    $(canvas).hide();
+    $(submit_btn).hide();
+    $(canvas).fadeIn(500);
+    $(submit_btn).fadeIn(500);
+
+    secretWordIndex = 0;
+    requestAnimationFrame(draw);
+  }
+}
+
+function draw() {
   let ctx = canvas.getContext('2d');
-  ctx.font = '24px "Flow Circular"';
   ctx.textBaseline = 'top';
-
-  // https://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   const lineHeight = 28;
-  const x = 5;
-  let y = 0;
+  const padding = 5;
+
+  let now = new Date();
+  let now_str = now.getDate().toString().padStart(2, '0') + '/' +
+    now.getMonth().toString().padStart(2, '0') + ' ' +
+    now.getHours().toString().padStart(2, '0') + ':' +
+    now.getMinutes().toString().padStart(2, '0');
+  ctx.font = '16px serif';
+  ctx.fillText(now_str, padding, 0);
+
+  ctx.font = '24px "Flow Circular"';
+  let spaceWidth = ctx.measureText(' ').width;
+
+  let x = padding;
+  let y = lineHeight;
+  let wordIndex = 0;
   let lines = text.split('\n');
-  let canvasLines = [];
   for (let line of lines) {
-    // y += lineHeight;
-    let currentLine = '';
     let words = line.split(' ');
     for (let word of words) {
-      let testLine = currentLine + word + ' ';
-      let metrics = ctx.measureText(testLine);
-      if (x + metrics.width > canvas.width) {
-        // ctx.fillText(currentLine, x, y);
-        canvasLines.push(currentLine);
-        currentLine = word + ' ';
-        // y += lineHeight;
+      if (wordIndex <= secretWordIndex) {
+        ctx.font = '24px "Flow Circular"';
       } else {
-        currentLine = testLine;
+        ctx.font = '24px serif';
       }
+      let wordWidth = ctx.measureText(word).width;
+      if (x + wordWidth + spaceWidth > canvas.width - padding) {
+        y += lineHeight;
+        x = padding;
+      }
+      ctx.fillText(word, x, y);
+      x += wordWidth + spaceWidth;
+      wordIndex += 1;
     }
-    canvasLines.push(currentLine);
   }
 
-  for (let canvasLine of canvasLines) {
-    ctx.fillText(canvasLine, x, y);
-    y += lineHeight;
+  if (secretWordIndex < wordIndex) {
+    secretWordIndex += 1;
+    requestAnimationFrame(draw);
+  } else {
+    let cropCanvas = document.createElement('canvas');
+    cropCanvas.width = canvas.width;
+    cropCanvas.height = y + lineHeight;
+    let cropCtx = cropCanvas.getContext('2d');
+    cropCtx.drawImage(canvas, 0, 0);
+    dataURL = cropCanvas.toDataURL();
   }
-
-  secret.append(canvas);
-
-  submit_btn = document.createElement('button');
-  submit_btn.type = 'button';
-  submit_btn.classList.add('button');
-  submit_btn.appendChild(document.createTextNode('submit'));
-  submit_btn.addEventListener('click', submitSecret);
-  secret.appendChild(submit_btn);
-
-  $(canvas).hide();
-  $(submit_btn).hide();
-  $(canvas).fadeIn(500);
-  $(submit_btn).fadeIn(500);
 }
 
 function submitSecret() {
@@ -98,14 +143,18 @@ function submitSecret() {
   $(submit_btn).fadeOut(500, () => submit_btn.remove());
 
   let image = new Image();
-  image.src = canvas.toDataURL();
-  image.classList.add('secret_img', 'animated', 'hidden');
-  $(image).css({
-    width: canvas.width * (Math.random() * 0.5 + 0.25),
-    top: Math.random() * window.innerHeight + 'px',
-    left: Math.random() * window.innerWidth + 'px'
-  });
+  image.src = dataURL;
+  image.classList.add('secret_img');
+  let style = `width: ${Math.floor(canvas.width * (Math.random() * 0.5 + 0.25))}px; ` +
+    `top: ${Math.floor(Math.random() * window.innerHeight)}px; ` +
+    `left: ${Math.floor(Math.random() * window.innerWidth)}px;`;
+  image.style = style;
   secrets.appendChild(image);
+  secret_imgs.push({
+    src: image.src,
+    style: style
+  });
+  localStorage.setItem('secret_imgs', JSON.stringify(secret_imgs));
   
   $(image).hide();
   $(image).fadeIn(500);
